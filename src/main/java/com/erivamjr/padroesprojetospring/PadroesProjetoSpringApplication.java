6511eb24bd272e48fcd7b961d419ac319f497f03
José Erivam <erivamdev@gmail.com>
2022-10-26 08:24:55 -0300

    :bug: fix:Add annotation @EnableFeignClients

--- src/main/java/com/erivamjr/padroesprojetospring/PadroesProjetoSpringApplication.java
+++ src/main/java/com/erivamjr/padroesprojetospring/PadroesProjetoSpringApplication.java
@@ -2,12 +2,15 @@ package com.erivamjr.padroesprojetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PadroesProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
+		System.out.println("FUNCIONANDO");
 	}
 
 }
