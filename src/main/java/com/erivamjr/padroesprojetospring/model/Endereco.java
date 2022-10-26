00245d5e5d59e10c479b11589a1530906cd43d99
José Erivam <erivamdev@gmail.com>
2022-10-26 08:21:46 -0300

    :bug: fix:repair entity Endereco

--- src/main/java/com/erivamjr/padroesprojetospring/model/Endereco.java
+++ src/main/java/com/erivamjr/padroesprojetospring/model/Endereco.java
@@ -1,7 +1,9 @@
 package com.erivamjr.padroesprojetospring.model;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 public class Endereco {
     @Id
     private String cep;
